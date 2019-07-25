fe0256d0290d70fbdd65011fc3993b5cc78b2e0a
Udi Oshi <udioshi@gmail.com>
2019-07-25 11:59:31 +0300

    Version 1.0.7
    - Added  getContext for ViewDataBinding class.
    - Added javadoc

--- src/main/java/com/github/udioshi85/tooly/Versions.kt
+++ src/main/java/com/github/udioshi85/tooly/Versions.kt
@@ -2,12 +2,27 @@ package com.github.udioshi85.tooly
 
 import android.os.Build
 
+/**
+ * Return True if is above/equal 21
+ */
 fun isLollipopOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
 
+/**
+ * Return True if is above/equal 23
+ */
 fun isMarshmelloOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
 
+/**
+ * Return True if is above/equal 24
+ */
 fun isNugatOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
 
+/**
+ * Return True if is above/equal 26
+ */
 fun isOreoOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
+/**
+ * Return True if is above/equal 28
+ */
 fun isPieOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
---
+++ src/main/java/com/github/udioshi85/tooly/ViewDataBinding.kt
@@ -0,0 +1,9 @@
+package com.github.udioshi85.tooly
+
+import android.content.Context
+import androidx.databinding.ViewDataBinding
+
+/**
+ * Simply getting the Context from the Binding root view.
+ */
+fun ViewDataBinding.getContext(): Context = this.root.context
