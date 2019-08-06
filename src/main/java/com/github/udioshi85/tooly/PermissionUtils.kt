9bdc37bd2afcad534560879064187801e3ef70f5
Udi Oshi <udioshi@gmail.com>
2019-08-06 17:19:17 +0300

    Version 1.0.8
    - Added request permission from fragment

--- src/main/java/com/github/udioshi85/tooly/PermissionUtils.kt
+++ src/main/java/com/github/udioshi85/tooly/PermissionUtils.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 
 fun hasAccessNetworkStatePermission(context: Context) = hasPermission(context, Manifest.permission.ACCESS_NETWORK_STATE)
 fun hasPermission(context: Context, permission: String): Boolean = ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
@@ -14,3 +15,4 @@ fun hasPermission(context: Context, permission: String): Boolean = ContextCompat
 /** External storage **/
 fun hasWriteExternalStoragePermission(context: Context): Boolean = hasPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 fun requestExternalStoragePermission(activity: Activity, requestCode: Int) = ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
+fun requestExternalStoragePermission(fragment: Fragment, requestCode: Int) = fragment.requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
