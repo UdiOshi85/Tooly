70b3bb22a2feb6746a9ca2fbf166894688dc2d11
Udi Oshi <udioshi@gmail.com>
2019-11-27 15:33:57 +0200

    Version 1.1.3
    - Added some java docs
    - Added 2 xml animations

--- src/main/java/com/github/udioshi85/tooly/extentions/View.kt
+++ src/main/java/com/github/udioshi85/tooly/extentions/View.kt
@@ -2,10 +2,22 @@ package com.github.udioshi85.tooly.extentions
 
 import android.view.View
 
+/**
+ * Sets the view visibility to View.GONE
+ */
 fun View.setGone() { this.visibility = View.GONE }
 
+/**
+ * Sets the view visibility to View.VISIBLE
+ */
 fun View.setVisible() { this.visibility = View.VISIBLE }
 
+/**
+ * Sets the view visibility to View.INVISIBLE
+ */
 fun View.setInvisible() { this.visibility = View.INVISIBLE }
 
+/**
+ * Set View.Visible / View.GONE according to isVisible
+ */
 fun View.setVisible(isVisible: Boolean) = if (isVisible) setVisible() else setGone()
