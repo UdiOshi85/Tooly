b4ccfbb5e3304468bebaabfe90388a18edcc4652
Udi Oshi <udioshi@gmail.com>
2019-12-10 11:15:04 +0200

    - Javadoc

--- src/main/java/com/github/udioshi85/tooly/CalendarUtils.kt
+++ src/main/java/com/github/udioshi85/tooly/CalendarUtils.kt
@@ -3,5 +3,9 @@ package com.github.udioshi85.tooly
 import com.github.udioshi85.tooly.extentions.getFormattedTime
 import java.util.*
 
+/**
+ * Example: Nov, 25
+ */
 const val FORMAT_MMM_DD = "MMM, dd"
+
 fun getFormattedTime(format: String): String = Calendar.getInstance().getFormattedTime(format)
