c8b53ec35ca8a3e5cd50a86e31d72a2ea43c04b2
Udi Oshi <udioshi@gmail.com>
2020-11-26 17:21:49 +0200

    Version 1.4

---
+++ src/main/java/com/github/udioshi85/tooly/CustomTypefaceSpan.kt
@@ -0,0 +1,15 @@
+package com.github.udioshi85.tooly
+
+import android.graphics.Typeface
+import android.text.TextPaint
+import android.text.style.MetricAffectingSpan
+
+class CustomTypefaceSpan(private val typeface: Typeface?) : MetricAffectingSpan() {
+    override fun updateDrawState(paint: TextPaint) {
+        paint.typeface = typeface
+    }
+
+    override fun updateMeasureState(paint: TextPaint) {
+        paint.typeface = typeface
+    }
+}
