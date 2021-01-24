23f96994a07f62a97873f25c88ea1555f8e4e0e6
Udi Oshi <udioshi@gmail.com>
2021-01-24 11:21:32 +0200

    Version 1.5.1

--- src/main/java/com/github/udioshi85/tooly/PermissionUtils.kt
+++ src/main/java/com/github/udioshi85/tooly/PermissionUtils.kt
@@ -12,8 +12,12 @@ fun hasAccessNetworkStatePermission(context: Context) = hasPermission(context, M
 fun hasPermission(context: Context, permission: String): Boolean = ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
 
 
-/** External storage **/
+/** External storage (Read & write) **/
 fun hasReadExternalStoragePermission(context: Context): Boolean = hasPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE)
 fun hasWriteExternalStoragePermission(context: Context): Boolean = hasPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-fun requestExternalStoragePermission(activity: Activity, requestCode: Int) = ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
-fun requestExternalStoragePermission(fragment: Fragment, requestCode: Int) = fragment.requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
+
+fun requestPermissionWriteExternalStorage(activity: Activity, requestCode: Int) = ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
+fun requestPermissionWriteExternalStorage(fragment: Fragment, requestCode: Int) = fragment.requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
+
+fun requestPermissionReadExternalStorage(activity: Activity, requestCode: Int) = ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), requestCode)
+fun requestPermissionReadExternalStorage(fragment: Fragment, requestCode: Int) = fragment.requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), requestCode)
